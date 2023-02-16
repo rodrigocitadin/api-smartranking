e31f0ed3544050814983abdac5c5ae325544b713
rodrigocitadin <citadinrodrigo@gmail.com>
2023-02-15 22:10:25 -0300

    feat: initializing mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { PlayersModule } from './players/players.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [PlayersModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:<o1mXoFpToUaGAc6V>@smartranking.zlxccgo.mongodb.net/?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+      },
+    ),
+    PlayersModule,
+  ],
   controllers: [],
   providers: [],
 })
