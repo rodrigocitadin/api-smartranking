2652861b07b305742de6a9703fc2ea7717141f03
rodrigocitadin <citadinrodrigo@gmail.com>
2023-02-15 22:46:50 -0300

    RESOLVING SOME TROUBLESOME

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,18 @@
 import { Module } from '@nestjs/common';
 import { PlayersModule } from './players/players.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ debug: true });
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://admin:<o1mXoFpToUaGAc6V>@smartranking.zlxccgo.mongodb.net/?retryWrites=true&w=majority',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-      },
-    ),
+    MongooseModule.forRoot(process.env.DB_KEY, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }),
     PlayersModule,
   ],
   controllers: [],
